310df029a4d8e8a6aab06731dc926d9f09232db4
David Torreiro Cotelo <davidtorreirocotelo@Mac-mini-de-Alberto.local>
2015-11-25 12:20:03 +0100

    update gemfile for heroku

--- Gemfile
+++ Gemfile
@@ -3,14 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
-# Use sqlite3 as the database for Active Record
-gem 'rails_12factor', group: :production do
-	gem 'pg'
-end
 
-
-
-gem 'sqlite3'
+# Using postgres
 
 
 # Use SCSS for stylesheets
@@ -51,8 +45,13 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'sqlite3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
+group :production do
+  gem 'pg',             '0.18.4'
+  gem 'rails_12factor'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,16 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ Article.create(title: 'Seguir así', body: 'Hola mi nombre es Pedro y me he enganchado a vuestro desarrollo, 
+ os sigo la pista en todas las redes sociales y cada vez que veo una hoja de diseño me fascina. Un saludo cracks.')
+
+ Article.create(title: 'Gran equipo', body: 'Buenos días, me llamo Laura y me parece que estáis haciendo un gran
+ trabajo y sois un buen grupo de profesionales, espero que consigáis vuestro objetivo. Chao')
+
+ Article.create(title: 'Tiempo de producción', body: 'Qué tal? Soy Alberto y tengo ya muchas ganas de poder hincanrle
+ el diente a Insecminator, cuanto estareis trabajando? Cuando pretendeis acabar? Espero que me respondáis lo más pronto
+ posible. Hasta otra.')
+
+ Article.create(title: 'Increíble idea', body: 'Al habla Carolina desde Barcelona, no sabía si contactar con vosotros
+ ya que suelo mantenerme en la sombra pero ha sido tal mi interés por el desarrollo que aquí estoy. Solo quería deciros
+ que me parece una brillante idea de juego, espero que se materialice pronto.')
